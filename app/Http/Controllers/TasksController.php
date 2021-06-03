7900a0074d12af82fd59a9a362c228e39daabc66
atsushi-yabui <atsushi-yabui@dac.co.jp>
2021-06-03 07:24:12 +0000

    task all

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        // メッセージ一覧を取得
+        $tasks = Task::all();
+
+        // メッセージ一覧ビューでそれを表示
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    // getでmtasks//createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $tasl = new Task;
+
+        // メッセージ作成ビューを表示
+        return view('task.create', [
+            'task' => $task,
+        ]);
+    }
+
+    // postでtasks/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        // メッセージを作成
+        $message = new Task;
+        $message->content = $request->content;
+        $message->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // getでtasks//idにアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        // idの値でメッセージを検索して取得
+        $message = Task::findOrFail($id);
+
+        // メッセージ詳細ビューでそれを表示
+        return view('task.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでtasks//id/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('task.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    // putまたはpatchでtasks//idにアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        // idの値でメッセージを検索して取得
+        $task= Tasl::findOrFail($id);
+        // メッセージを更新
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // deleteでtasks/idにアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+}
